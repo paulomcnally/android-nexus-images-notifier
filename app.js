9471079fb20d86f70085ff945b5f5b2264d33974
Paulo McNally <paulomcnally@gmail.com>
2014-11-06 01:44:06 -0600

    remove moment

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var moment = require('moment');
 var Lib = require('./lib');
 var lib = new Lib();
-moment.locale('es');
 
 var CronJob = require('cron').CronJob;
 new CronJob('1 10 * * * *', function() {
@@ -13,13 +11,12 @@ http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   lib.getCache(function(error, cache) {
     var output = '';
-    var date = moment().format('llll');
     if (error) {
       output = error;
     }
     else {
       output = cache
     }
-    res.end(output  + '<br />' + date);
+    res.end(output);
   });
 }).listen(process.env.PORT || 1337, null);
