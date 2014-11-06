a5837cdbe234d9a38aaa2148f322bbe9d8bc673c
Paulo McNally <paulomcnally@gmail.com>
2014-11-06 01:15:52 -0600

    adde date

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ new CronJob('1 10 * * * *', function() {
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   lib.getCache(function(error, cache) {
+    var d = new Date();
     if (error) {
-      res.end(error);
+      res.end(error + '\n' + d.toString());
     }
     else {
-      res.end(cache);
+      res.end(cache  + '\n' + d.toString());
     }
   });
 }).listen(process.env.PORT || 1337, null);
